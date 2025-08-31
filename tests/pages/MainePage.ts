e93010f04d974b1f79c22543c3a3db172454de5e
Artem <citif75244@yahoo.com>
2025-08-31 22:27:20 +0300

    add main page tests

--- tests/pages/MainePage.ts
+++ tests/pages/MainePage.ts
@@ -7,6 +7,12 @@ export class MainPage extends BasePage {
   private readonly menuLocator: Locator;
   private readonly modalNewRegLocator: Locator;
   private readonly modalNewRegButtonClosedLocator: Locator;
+  private readonly headerAddButton: Locator;
+  private readonly headerAddButtonListLocator: Locator;
+  private readonly headerNotificationsButton: Locator;
+  private readonly headerNotificationsPopupLocator: Locator;
+  private readonly headerLoginButton: Locator;
+  private readonly modalLoginOrRegister: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -15,26 +21,69 @@ export class MainPage extends BasePage {
     this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
     this.modalNewRegLocator = this.page.locator('.wdp-popup-module__header');
     this.modalNewRegButtonClosedLocator = this.modalNewRegLocator.locator('button');
+    this.headerAddButton = this.page.getByRole('button', { name: 'Добавить' });
+    this.headerAddButtonListLocator = this.page.locator('.wdp-header-right-module__uploader ul');
+    this.headerNotificationsButton = this.page.getByRole('button', { name: 'Уведомления' });
+    this.headerNotificationsPopupLocator = this.page.locator(
+      '.wdp-notifications-popup-module__wrapper',
+    );
+    this.headerLoginButton = this.page.getByRole('button', { name: 'Вход и регистрация' });
+    this.modalLoginOrRegister = this.page
+      .locator('iframe[title="Multipass"]')
+      .contentFrame()
+      .locator('div[role="form"]');
   }
   async open() {
     await this.page.goto('https://rutube.ru/');
   }
 
   async headerHasCorrectAriaSnapshot() {
-    await expect(this.headerLocator).toMatchAriaSnapshot();
+    await expect(this.headerLocator).toMatchAriaSnapshot({ name: 'hederAriaSnapshot.yml' });
   }
 
   async categoriesTabsHasCorrectAriaSnapshot() {
-    await expect(this.categoriesTabLocator).toMatchAriaSnapshot();
+    await expect(this.categoriesTabLocator).toMatchAriaSnapshot({
+      name: 'catigoriesAriaSnapshot.yml',
+    });
   }
 
   async menuHasCorrectAriaSnapshot() {
-    await expect(this.menuLocator).toMatchAriaSnapshot();
+    await expect(this.menuLocator).toMatchAriaSnapshot({ name: 'menuAriaSnapshot.yml' });
   }
 
   async closedModalNewReg() {
     if (await this.modalNewRegLocator.isVisible()) {
       await this.modalNewRegButtonClosedLocator.click();
     }
+  }
+
+  async openAddPopupList() {
+    this.headerAddButton.click();
+  }
+
+  async openNotificationsPopupList() {
+    this.headerNotificationsButton.click();
+  }
+
+  async openLoginModal() {
+    this.headerLoginButton.click();
+  }
+
+  async addPopupListHasCorrectAriaSnapshot() {
+    await expect(this.headerAddButtonListLocator).toMatchAriaSnapshot({
+      name: 'addButtonList.yml',
+    });
+  }
+
+  async notificationsPopupHasCorrectAriaSnapshot() {
+    await expect(this.headerNotificationsPopupLocator).toMatchAriaSnapshot({
+      name: 'notificationsPopup.yml',
+    });
+  }
+
+  async modalLoginOrRegisterHasCorrectAriaSnapshot() {
+    await expect(this.modalLoginOrRegister).toMatchAriaSnapshot({
+      name: 'modalLoginOrRegister.yml',
+    });
   }
 }
--- tests/specs/unauthorizaded/mainPage.spec.ts
+++ tests/specs/unauthorizaded/mainPage.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '../../fixtures/fixtures';
-import { MainPage } from '../../pages/MainePage';
 
 test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
@@ -11,4 +10,21 @@ test('Проверка доступности элементов меню', asyn
 
 test('Проверка доступности элементов табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов списка добовления контента', async ({ mainPage }) => {
+  await mainPage.openAddPopupList();
+  await mainPage.addPopupListHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов поп-апа уведомлений', async ({ mainPage }) => {
+  await mainPage.openNotificationsPopupList();
+  await mainPage.notificationsPopupHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов модального окна авторизации и регистрации', async ({
+  mainPage,
+}) => {
+  await mainPage.openLoginModal();
+  await mainPage.modalLoginOrRegisterHasCorrectAriaSnapshot();
 });
